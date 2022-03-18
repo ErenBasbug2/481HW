e11e3f8a49d996f7e0b21c9c0bfa8eba264b191c
ErenBasbug2 <76902661+ErenBasbug2@users.noreply.github.com>
2022-03-18 16:21:57 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,1 +1,11 @@
-
+public class App {
+	public static boolean checkIfMultipleOfNumber(int array[], int number,int index) {
+		if(array[index] % number == 0)
+		{
+			return true;
+		}
+		return false;
+		
+	}
+			
+}
